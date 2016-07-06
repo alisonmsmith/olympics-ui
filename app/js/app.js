48c48b9b63519e630aa29471137a0967a8e2b02e
Alison Smith <alison.smith.m@gmail.com>
2016-07-06 14:39:42 -0400

    updates

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,35 @@
-angular.module('olympics', [
+var app = angular.module('olympics', [
   'ngRoute',
+  'ngMaterial',
   'olympics.controllers',
   'olympics.services',
   'olympics.directives'
 ]);
+
+/*var app = angular.module('olympics', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ngMaterial',
+  'ui.router',
+  'ui.bootstrap',
+  'angularSpinners'
+ // 'ngAnimate'
+]);*/
+
+app.config(function($routeProvider, $mdThemingProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl: 'app/views/overview.html',
+        controller: 'MainCtrl'
+      })
+        .otherwise({
+          redirectTo: '/'
+        });
+
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue-grey')
+    .accentPalette('pink');
+
+});
